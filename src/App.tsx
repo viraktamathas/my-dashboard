4d14462dc3869d22e281367b64fc7487138d1f91
Amit Viraktamath <amit.viraktamath@nih.gov>
2025-07-08 00:42:36 -0400

    Add src/App.tsx from react-app template

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>my-dashboard</h1>
+        <p>building a modern web dashboard</p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
